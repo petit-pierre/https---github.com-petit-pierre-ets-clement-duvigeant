caafea83f88a3ad2fa17eb37dc1109a6c8dcf771
petit-pierre <aubree.pierre2@gmail.com>
2024-09-10 15:21:58 +0200

    V0.01 header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,12 +2,37 @@ import { Link } from "react-scroll";
 import "./header.css";
 
 function Header() {
+  function clicked(evt) {
+    const links = document.querySelectorAll(".active");
+    if (links !== null) {
+      for (let link of links) {
+        link.classList.remove("active");
+      }
+    }
+    if (evt.target.parentElement.parentElement.className.includes("button")) {
+      evt.target.parentElement.parentElement.classList.add("active");
+    } else {
+      evt.target.parentElement.parentElement.parentElement.classList.add(
+        "active"
+      );
+    }
+  }
+  function clickedLogo() {
+    const links = document.querySelectorAll(".active");
+    if (links !== null) {
+      for (let link of links) {
+        link.classList.remove("active");
+      }
+    }
+  }
   return (
     <div className="headerField">
       <div className="header">
         <div className="top">
-          <div className="logo">
-            <img src="./assets/logo.png" alt="logo"></img>
+          <div className="logo" onClick={(evt) => clickedLogo()}>
+            <Link to="coucou" smooth="true" onClick={(evt) => clickedLogo()}>
+              <img src="./assets/logo.png" alt="logo"></img>
+            </Link>
           </div>
           <div className="name">
             <h1>Ets Clément Duvigeant</h1>
@@ -24,24 +49,37 @@ function Header() {
               <img src="./assets/tel.png" alt="logo"></img>
               <p>07.81.11.54.30</p>
             </a>
-            <div className="button active">
-              <Link to="coucou" smooth={true}>
-                <p>Eau chaude</p>
+            <div className="button red">
+              <Link
+                to="coucou"
+                smooth={true}
+                name="prout"
+                onClick={(evt) => clicked(evt)}
+              >
+                <div>
+                  <p>Eau chaude</p>
+                </div>
               </Link>
             </div>
-            <div className="active button">
-              <Link to="coucou" smooth={true}>
-                <p>V.M.C</p>
+            <div className="button blue">
+              <Link to="coucou" smooth={true} onClick={(evt) => clicked(evt)}>
+                <div>
+                  <p>V.M.C</p>
+                </div>
               </Link>
             </div>
-            <div className="button active">
-              <Link to="coucou" smooth={true}>
-                <p>Chauffage</p>
+            <div className="button red">
+              <Link to="coucou" smooth={true} onClick={(evt) => clicked(evt)}>
+                <div>
+                  <p>Chauffage</p>
+                </div>
               </Link>
             </div>
-            <div className="button active">
-              <Link to="coucou" smooth={true}>
-                <p>Climatisation</p>
+            <div className="button blue">
+              <Link to="coucou" smooth={true} onClick={(evt) => clicked(evt)}>
+                <div>
+                  <p>Climatisation</p>
+                </div>
               </Link>
             </div>
           </nav>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,6 +3,16 @@ import "./home.css";
 function Home() {
   return (
     <div className="home">
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1480966.5153097906!2d2.197417!3d43.533353!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12ae15152d746de1%3A0x9292b5f1e813a3c!2sCLEMENT%20DUVIGEANT%20ESCOUSSENS!5e0!3m2!1sfr!2sfr!4v1725951802595!5m2!1sfr!2sfr"
+        width="600"
+        height="450"
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+        className="map"
+      ></iframe>
+
       <p>coucou 1</p>
       <p>coucou</p>
       <p>coucou</p>
